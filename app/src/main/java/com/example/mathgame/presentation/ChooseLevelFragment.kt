498bf619a8a927ddd444bab5f43b8d098d3918fa
lils <lilsolkaryan@gmail.com>
2024-08-04 15:25:18 +0400

    Work with ViewBinding

--- app/src/main/java/com/example/mathgame/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/mathgame/presentation/ChooseLevelFragment.kt
@@ -6,13 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mathgame.R
+import com.example.mathgame.databinding.FragmentChooseLevelBinding
 
 class ChooseLevelFragment : Fragment() {
-
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding : FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding is null")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
