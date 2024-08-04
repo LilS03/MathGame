e87c9e5165dccfacfa1dc9cbaffaf78e3f008b30
lils <lilsolkaryan@gmail.com>
2024-08-04 17:23:12 +0400

    Navigation

--- app/src/main/java/com/example/mathgame/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/mathgame/presentation/ChooseLevelFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mathgame.R
 import com.example.mathgame.databinding.FragmentChooseLevelBinding
+import com.example.mathgame.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
     private var _binding: FragmentChooseLevelBinding? = null
@@ -21,10 +22,36 @@ class ChooseLevelFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            buttonLevelTest.setOnClickListener{
+                launchGameFragment(Level.TEST)
+            }
+            buttonLevelEasy.setOnClickListener{
+                launchGameFragment(Level.EASY)
+            }
+            buttonLevelNormal.setOnClickListener{
+                launchGameFragment(Level.NORMAL)
+            }
+            buttonLevelHard.setOnClickListener{
+                launchGameFragment(Level.HARD)
+            }
+        }
+    }
+    private fun launchGameFragment(level: Level){
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(GameFragment.NAME)
+            .commit()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    companion object{
+        const val NAME = "ChooseLevelFragment"
+        fun newInstance(): ChooseLevelFragment{
+            return ChooseLevelFragment()
+        }
     }
 }
--- app/src/main/java/com/example/mathgame/presentation/GameFragment.kt
+++ app/src/main/java/com/example/mathgame/presentation/GameFragment.kt
@@ -7,12 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mathgame.R
 import com.example.mathgame.databinding.FragmentGameBinding
-import com.example.mathgame.databinding.FragmentWelcomeBinding
+import com.example.mathgame.domain.entity.GameResult
+import com.example.mathgame.domain.entity.GameSettings
+import com.example.mathgame.domain.entity.Level
 
 class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
-    private val binding : FragmentGameBinding
+    private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding is null")
+    private lateinit var level: Level
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,12 +23,53 @@ class GameFragment : Fragment() {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tvOption1.setOnClickListener {
+            launchGameFinishedFragment(GameResult(
+                true,
+                0,
+                0,
+                GameSettings(0,
+                    0,
+                    0,
+                    0)
+            )
+            )
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+        private const val KEY_LEVEL = "level"
+        const val NAME = "GameFragment"
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
